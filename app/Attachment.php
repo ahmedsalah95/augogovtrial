2c1cda2ff4d2338f209c92ffc107fa7f9ea59b93
ahmedsalah95 <ahmedsalah9570@gmail.com>
2019-03-02 22:38:20 +0200

    instance attachment

--- app/Attachment.php
+++ app/Attachment.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Attachment extends Model
 {
-    //
+    public function Instance_Attachment()
+    {
+        return $this->hasMany('App\Instance_Attachment','attachment_id','id');
+    }
 }
---
+++ database/migrations/2019_03_02_143905_create_instance__attachments_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInstanceAttachmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('instance__attachments', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('attachment_id')->unsigned();
+            $table->foreign('attachment_id')->references('id')->on('attachments');
+            $table->integer('cat')->nullable();
+            $table->integer('ORG_id')->nullable();
+            $table->integer('Archived')->nullable();
+            $table->integer('Received')->nullable();
+            $table->integer('deleted')->nullable();
+            $table->string('mandatory_optional')->nullable();
+            $table->integer('archive_document_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('instance__attachments');
+    }
+}

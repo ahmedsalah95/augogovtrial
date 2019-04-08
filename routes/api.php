b90d8bc4a811214454b2bb8834d90973c3ead7a1
MouhammedElshaaer <mouhammedelshaaer@stud.fci-cu.edu.eg>
2019-04-08 20:20:17 +0200

    transaction addition

--- routes/api.php
+++ routes/api.php
@@ -23,16 +23,15 @@ Route::post('/citizen/insert','LookupController@insertCitizen');
 Route::post('/buildingType/insert','LookupController@insertBuildingType');
 Route::post('/distinctionType/insert','LookupController@insertDistinctionType');
 Route::post('/assignationType/insert','LookupController@insertAssignationType');
-Route::post('/document/insert','LookupController@fetchDocuments');
-Route::post('/fees/insert','LookupController@fetchFees');
+Route::post('/documents/fetch','LookupController@fetchDocuments');
+Route::post('/fees/fetch','LookupController@fetchFees');
 Route::post('/payment/insert','LookupController@insertPayment');
 Route::post('/request/insert','LookupController@insertRequest');
 Route::post('/addressItem/insert','addressStructureController@insertAddressItem');
 Route::post('/addressItemInstance/insert','addressStructureController@insertAddressItemInstance');
 Route::post('/addressStructure/insert','addressStructureController@insertAddressStructure');
 Route::post('/requests/get','LookupController@getRequests');
-Route::post('/documents/get','LookupController@getDocuments');
-Route::post('/fees/get','LookupController@getFees');
+
 Route::post('/requestById/get','LookupController@getRequestByID');
 Route::post('/documentsByReqId/get','LookupController@getDocumentsByReqId');
 Route::post('/feesByReqId/get','LookupController@getFeesByRequestId');
@@ -41,7 +40,8 @@ Route::post('/addressItems/get','addressStructureController@getAddressItems');
 Route::post('/addressItemsInstance/get','addressStructureController@getAddressItemsInstance');
 Route::post('/addressStructure/get','addressStructureController@getAddressStructure');
 
-
+Route::get('/documents/get','LookupController@getDocuments');
+Route::get('/fees/get','LookupController@getFees');
 
 Route::post('/user/login','UserController@login');
 
@@ -53,6 +53,7 @@ Route::post('/lus/add','LusController@LusAdd');
 Route::post('/lus/setDecision','LusController@setDecision');
 
 
+Route::post('/transactions/fetch','requestController@fetchTransactions');
 Route::post('/createReq','requestController@createRequest');
 Route::post('/reqFees','requestController@requestFees');
 Route::post('/reqFees','requestController@form');

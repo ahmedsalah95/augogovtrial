518e80be83eea79064a4c516b14750d0f0e86bc6
ahmedsalah95 <ahmedsalah9570@gmail.com>
2019-04-17 22:20:36 +0200

    fixing some points and testing ionic

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 */
+Route::post('/register','UserController@register');
 Route::post('/citizen/insert','LookupController@insertCitizen');
 Route::get('/citizens/get','LookupController@getCitizens');
 Route::post('/buildingType/insert','LookupController@insertBuildingType');

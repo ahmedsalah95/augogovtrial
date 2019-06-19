88b8d35878b7d9d792c94d73d5ffd69a6280a311
MouhammedElshaaer <mouhammedelshaaer@stud.fci-cu.edu.eg>
2019-06-19 03:40:16 +0200

    removing redundant routes - fixing already existing addresses related function and adding also some functions - migration file modifications related to addresses

--- app/Http/Controllers/addressStructureController.php
+++ app/Http/Controllers/addressStructureController.php
@@ -11,93 +11,122 @@ use Illuminate\Http\Request;
 class addressStructureController extends Controller
 {
 
+    public function fetchAddressItems(Request $request){
 
-    public function insertAddressItem(Request $request)
+        dump($request->data);
+
+        foreach ($request->data as $addressItem)
+        {
+            if($addressItem["new_address_item"])
+            {
+                $this->insertAddressItem($addressItem);
+            }
+            else if($addressItem["deleted"]){
+                $this->deleteAddressItem($addressItem["id"]);
+            }
+        }
+
+    }
+
+    public function insertAddressItem($addressItem)
     {
-        $this->validate($request,[
-
-            'item_name' =>'required',
-            'item_cost'=>'required',
-            'item_digit'=>'required'
-
-        ]);
 
         $add = new Address_item();
-        $add->item_name = $request->item_name;
-        $add->item_cost = $request->item_cost;
-        $add->item_digit = $request->item_digit;
+        $add->name = $addressItem["name"];
+        $add->code = $addressItem["code"];
+        $add->digit = $addressItem["digit"];
 
         $add->save();
 
-        $authorized = Address_item::where('id',$add->id)->get();
-        return response()->json(['address item',$authorized],200);
-
     }
 
-    public function insertAddressItemInstance(Request $request)
+    public function getAddressItems()
     {
-        $this->validate($request,[
+        $addressItems = Address_item::all();
+        return response()->json(['address_items' => $addressItems],200);
+    }
 
-            'item_id' =>'required',
-            'instance_name'=>'required',
-            'instance_code'=>'required'
+    public function deleteAddressItem($id){
+        $addressItem = Address_item::findOrFail($id);
+        $addressItem->delete();
+    }
 
-        ]);
+    public function fetchAddressItemsInstances(Request $request){
+        dump($request->data);
+
+        foreach ($request->data as $addressItemInstance)
+        {
+            if($addressItemInstance["new_address_item_instance"])
+            {
+                $this->insertAddressItemInstance($addressItemInstance);
+            }
+            else if($addressItemInstance["deleted"]){
+                $this->deleteAddressItemInstance($addressItemInstance["id"]);
+            }
+        }
+    }
+
+    public function insertAddressItemInstance($addressItemInstance)
+    {
 
         $add = new Address_item_instance();
-        $add->item_id = $request->item_id;
-        $add->instance_name = $request->instance_name;
-        $add->instance_code = $request->instance_code;
+        $add->address_item_id = $addressItemInstance["address_item"]["id"];
+        $add->name = $addressItemInstance["name"];
+        $add->code = $addressItemInstance["address_item"]["code"];
 
         $add->save();
 
-        $authorized = Address_item_instance::where('id',$add->id)->get();
-        return response()->json(['address item instance',$authorized],200);
+    }
+
+    public function getAddressItemsInstances()
+    {
+        $addressItemsInstances = Address_item_instance::all();
+        return response()->json(['address_items_instances' => $addressItemsInstances],200);
+    }
+
+    public function deleteAddressItemInstance($id){
+        $addressItemInstance = Address_item_instance::findOrFail($id);
+        $addressItemInstance->delete();
+    }
+
+    public function fetchAddressStructures(Request $request){
+        dump($request->data);
+
+        foreach ($request->data as $addressStructure)
+        {
+            if($addressStructure["new_address_structure"])
+            {
+                $this->insertAddressStructure($addressStructure);
+            }
+            else if($addressStructure["deleted"]){
+                $this->deleteAddressStructure($addressStructure["id"]);
+            }
+        }
+    }
+
+    public function insertAddressStructure($addressStructure)
+    {
+
+        $newAddressStructure = new Address_structure();
+        $newAddressStructure->address_item_instance_id = $addressStructure["address_item_instance"]["id"];
+        $newAddressStructure->parent_id = $addressStructure["parent"]["id"];
+        $newAddressStructure->acc_code = $addressStructure["accumulated_code"];
+        $newAddressStructure->acc_address = $addressStructure["accumulated_address"];
+
+        $newAddressStructure->save();
 
     }
 
-    public function insertAddressStructure(Request $request)
+    public function getAddressStructures()
     {
-        $this->validate($request,[
+        $addressStructures = Address_structure::all();
 
-            'instance_id' =>'required',
-            'structure_id_parent'=>'required',
-            'acc_code'=>'required',
-            'acc_address'=>'required'
-
-        ]);
-
-        $add = new Address_structure();
-        $add->instance_id = $request->instance_id;
-        $add->structure_id_parent = $request->structure_id_parent;
-        $add->acc_code = $request->acc_code;
-        $add->acc_address = $request->acc_address;
-
-        $add->save();
-
-
-        $authorized = Address_structure::where('id',$add->id)->get();
-        return response()->json(['address Structure',$authorized],200);
-
+        return response()->json(['address_structures' => $addressStructures],200);
     }
 
-    public function getAddressItems(Request $request)
-    {
-        $items = Address_item::where('id',$request->id)->get();
-
-        return response()->json(['address items', $items],200);
-    }
-    public function getAddressItemsInstance(Request $request)
-    {
-        $instance = Address_item_instance::where('id',$request->id)->get();
-
-        return response()->json(['address items instance', $instance],200);
-    }
-    public function getAddressStructure(Request $request)
-    {
-        $address = Address_structure::where('id',$request->id)->get();
-
-        return response()->json(['address structure', $address],200);
+    public function deleteAddressStructure($id){
+        $addressStructure = Address_structure::findOrFail($id);
+        $addressStructure->delete();
     }
 
 }

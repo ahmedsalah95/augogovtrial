89a292bc106bb22a197c6d8a232d7da002e4301c
ahmedsalah95 <ahmedsalah9570@gmail.com>
2019-06-21 23:40:52 +0200

    get user function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -158,6 +158,14 @@ class UserController extends Controller
         return response()->json($data, 200);
     }
 
+    //addded by ahmed salah
+    public function getUser(Request $request)
+    {
+        $user = User::find($request->id);
+        return response()->json([$user],200);
+
+    }
+
     public function register(Request $request)
     {
         $validator = Validator::make($request->all(), [
